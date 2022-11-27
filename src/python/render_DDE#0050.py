696eb0ef6b63c2bdacf4a295b5486c54979b29e2
N(Natukikazemizo) <kmaeyama@ms4.megaegg.ne.jp>
2022-11-27 12:58:59 +0900

    DDEのレンダリング用の.pyの作成着手
    カメラ指定機能等は外した。代わりに，指定フレーム範囲レンダー前後に，コレクションを表示して隠す機能を追加した。

---
+++ src/python/render_DDE#0050.py
@@ -0,0 +1,69 @@
+# !BPY
+# -*- coding: UTF-8 -*-
+# Render
+#
+# Render with hide/show Collections
+#
+# 2022.11.25 Natukikazemizo
+#
+
+import bpy
+import os
+import math
+
+# CONSTANTS
+CURRENT_SCENE = "Root"
+TEST_MODE = True
+test_print_cnt = 0
+TEST_PATH = "//..\..\..\..\\renderResults\\DDE#0050_Test\\Test_"
+RESULT_PATH = "//..\..\..\..\\renderResults\\DDE#0050\\DDE0050_"
+FRAME_STEP = 1
+FPS = 24
+CAMERA = "Camera.Main"
+
+def hide_collection(collection_name, hide):
+    bpy.data.objects[collection_name].hide_viewport = hide
+    bpy.data.objects[collection_name].hide_render = hide
+
+def render(frame_start, frame_end, show_collections, hide_collections):
+    for collection_name in show_collections:
+        hide_collection(collection_name, False)
+    
+    bpy.context.scene.render.fps = FPS
+    bpy.context.scene.frame_step = FRAME_STEP
+    bpy.context.scene.frame_start = frame_start
+    if TEST_MODE:
+        bpy.context.scene.frame_end = frame_start
+        bpy.data.scenes[CURRENT_SCENE].render.filepath = TEST_PATH
+    else:
+        bpy.context.scene.frame_end = frame_end
+        bpy.data.scenes[CURRENT_SCENE].render.filepath = RESULT_PATH
+
+    bpy.context.scene.camera = bpy.data.objects[CAMERA]
+    print("#### Render Start  #### frame:" + str(frame_start) + \
+        "-" +str(frame_end))
+    bpy.ops.render.render(animation=True)
+
+    for collection_name in hide_collections:
+        hide_collection(collection_name, True)
+
+    print("#### Render End    #### frame:" + str(frame_start) + \
+        "-" + str(frame_end))
+
+
+    
+    
+
+# main
+
+print("######## START ########")
+
+#bpy.context.window.screen = bpy.data.screens['Render']
+
+bpy.context.scene.render.resolution_percentage = 100
+
+render(0, 0, [], ["FeatureTree", "Paintbrush", "PaintPlates", "WarnedSRC", "SVN", "RelatedDocuments", "HighlightedSourceCode"])
+
+print("######## END   ########")
+
+
