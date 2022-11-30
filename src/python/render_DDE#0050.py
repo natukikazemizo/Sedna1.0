08473b9dee559f14a4f61be27f3f193f273a47c8
N(Natukikazemizo) <kmaeyama@ms4.megaegg.ne.jp>
2022-11-30 20:23:42 +0900

    マーカーとフレーム数基準でレンダリングするように修正した。

--- src/python/render_DDE#0050.py
+++ src/python/render_DDE#0050.py
@@ -14,6 +14,7 @@ import math
 # CONSTANTS
 CURRENT_SCENE = "Root"
 TEST_MODE = True
+ENABLE_RENDER = False
 test_print_cnt = 0
 TEST_PATH = "//..\..\..\..\\renderResults\\DDE#0050_Test\\Test_"
 RESULT_PATH = "//..\..\..\..\\renderResults\\DDE#0050\\DDE0050_"
@@ -22,10 +23,16 @@ FPS = 24
 CAMERA = "Camera.Main"
 
 def hide_collection(collection_name, hide):
-    bpy.data.objects[collection_name].hide_viewport = hide
-    bpy.data.objects[collection_name].hide_render = hide
+    bpy.data.collections[collection_name].hide_viewport = hide
+    bpy.data.collections[collection_name].hide_render = hide
 
-def render(frame_start, frame_end, show_collections, hide_collections):
+def render(marker_name, frames, show_collections, hide_collections):
+    if marker_name not in bpy.context.scene.timeline_markers:
+        print("marker not found:" + marker_name)
+        return
+    frame_start = bpy.context.scene.timeline_markers[frame_name].frame
+    frame_end = frame_start + frames - 1
+    
     for collection_name in show_collections:
         hide_collection(collection_name, False)
     
@@ -40,9 +47,11 @@ def render(frame_start, frame_end, show_collections, hide_collections):
         bpy.data.scenes[CURRENT_SCENE].render.filepath = RESULT_PATH
 
     bpy.context.scene.camera = bpy.data.objects[CAMERA]
-    print("#### Render Start  #### frame:" + str(frame_start) + \
+    print("#### Render Start  #### " + marker_name + "frame:" + str(frame_start) + \
         "-" +str(frame_end))
-    bpy.ops.render.render(animation=True)
+        
+    if ENABLE_RENDER:
+        bpy.ops.render.render(animation=True)
 
     for collection_name in hide_collections:
         hide_collection(collection_name, True)
@@ -62,7 +71,35 @@ print("######## START ########")
 
 bpy.context.scene.render.resolution_percentage = 100
 
-render(0, 0, [], ["FeatureTree", "Paintbrush", "PaintPlates", "WarnedSRC", "SVN", "RelatedDocuments", "HighlightedSourceCode"])
+# 00 INIT
+render("0000_Init", 0, [], ["FeatureTree", "Paintbrush", "PaintPlates", "WarnedSRC", \
+    "SVN", "RelatedDocuments", "HighlightedSourceCode", "SyncSrcAndTree"])
+
+# 01 N
+render("0010_N", 300, [], [])
+# 02 N call DDE
+render("0020_N_call_DDE", 250, [], [])
+# 03 FeatureTree
+render("0030_FeatureTree", 600, ["FeatureTree"], ["FeatureTree"])
+# 04 Uninstall DDE
+render("0040_Uninstall_DDE", 700, [], [])
+# 05 Highlighted SRC
+render("0050_Highlighted_SRC", 300, ["HighlightedSourceCode"], ["HighlightedSourceCode"])
+# 06 SVN
+render("0060_SVN", 300, ["SVN"], ["SVN"])
+# 07 Emotional Exception
+render("0070_EmotionalException", 300, [], [])
+# 08 Sync Code And Tree
+render("0080_SyncCodeTree", 300, ["SyncSrcAndTree"], ["SyncSrcAndTree"])
+# 09 Warned SRC
+render("0090_WarnedSRC", 400, ["WarnedSRC"], ["WarnedSRC"])
+# 10 N And DDE
+render("0100_N_AndDDE", 300, [], [])
+# 11 DDE CHange
+render("0110_DDE_Transformation", 300, [], [])
+# 12 Projects
+render("0120_Projects", 600, [], [])
+
 
 print("######## END   ########")
 
