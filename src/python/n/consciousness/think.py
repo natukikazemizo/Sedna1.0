9e109c641feffb24325337162bd86506283e607d
N(Natukikazemizo) <kmaeyama@ms4.megaegg.ne.jp>
2024-04-16 20:58:53 +0900

    Nの思考を実装するコードのコーディング着手

---
+++ src/python/n/consciousness/think.py
@@ -0,0 +1,19 @@
+#!BPY
+# -*- coding: UTF-8 -*-
+# Implement N's think
+#
+# 2027.09.10 N-mizo(Natukikazemizo)
+
+import os
+import utils_log
+import math
+
+# init logger
+global logger
+logger = utils_log.Util_Log(os.path.basename(__file__))
+
+logger.start()
+
+    
+
+logger.end()
