2fefa7e28e6f157c1a0b3b3fd778120454ea1e5d
N(Natukikazemizo) <kmaeyama@ms4.megaegg.ne.jp>
2024-05-15 19:11:50 +0900

    引数修正
    フレーム数を指定する方式から、最終フレームのマーカーを探す方式に変更した。

--- src/python/render_DDE#0050.py
+++ src/python/render_DDE#0050.py
@@ -31,12 +31,16 @@ def hide_collection(collection_name, hide):
     bpy.data.collections[collection_name].hide_viewport = hide
     bpy.data.collections[collection_name].hide_render = hide
 
-def render(marker_name, frames, show_collections, hide_collections):
-    if marker_name not in bpy.context.scene.timeline_markers:
-        print("marker not found:" + marker_name)
+def render(start_marker_name, end_marker_name, show_collections, hide_collections):
+    if start_marker_name not in bpy.context.scene.timeline_markers:
+        print("start marker not found:" + start_marker_name)
         return
-    frame_start = bpy.context.scene.timeline_markers[marker_name].frame
-    frame_end = frame_start + frames - 1
+    if end_marker_name not in bpy.context.scene.timeline_markers:
+        print("end marker not found:" + end_marker_name)
+        return
+
+    frame_start = bpy.context.scene.timeline_markers[start_marker_name].frame
+    frame_end = bpy.context.scene.timeline_markers[end_marker_name].frame
     
     for collection_name in show_collections:
         hide_collection(collection_name, False)
@@ -52,7 +56,7 @@ def render(marker_name, frames, show_collections, hide_collections):
         bpy.data.scenes[SCENE_NAME].render.filepath = RESULT_PATH
 
     bpy.context.scene.camera = bpy.data.objects[CAMERA]
-    print("#### Render Start  #### " + marker_name + " frame:" + str(frame_start) + \
+    print("#### Render Start  #### " + start_marker_name + " frame:" + str(frame_start) + \
         "-" +str(frame_end))
         
     if ENABLE_RENDER:
@@ -77,34 +81,7 @@ print("######## START ########")
 bpy.context.scene.render.resolution_percentage = 100
 
 
-# 0000 Init 
-#render("0000_Init", 0, [], ["FeatureTree", "Paintbrush", "PaintPlates", "WarnedSRC", \
-#    "VCS", "RelatedDocuments", "HighlightedSourceCode", "SyncSrcAndTree"])
-# 0010 N -> Rejected?
-# render("0010_N", 300, [], [])
-# 0020 N call DDE
-# render("0020_N_call_DDE", 250, [], [])
-# 0030 FeatureTree
-# render("0030_FeatureTree", 600, ["FeatureTree"], ["FeatureTree"])
-# 0040 Uninstall DDE
-# render("0040_Uninstall_DDE", 700, [], [])
-# 0050 Highlighted SRC
-# render("0050_Highlighted_SRC", 300, ["HighlightedSourceCode"], ["HighlightedSourceCode"])
-# 0060 SVN
-# render("0060_SVN", 500, ["SVN","vcs"], ["SVN","vcs"])
-# 0070 Call tree & editor
-# 0070_CallTreeAndEditor
-render("0070_CallTreeAndEditor", 300, [], ["VCS.001"])
-# 0080 Sync Code And Tree
-# render("0080_SyncCodeTree", 300, ["SyncSrcAndTree"], ["SyncSrcAndTree"])
-# 0090 Warned SRC
-# render("0090_WarnedSRC", 400, ["WarnedSRC"], ["WarnedSRC"])
-# 0100 N And DDE
-# render("0100_N_And_DDE", 300, [], [])
-# 0110 DDE CHange
-# render("0110_DDE_Transformation", 300, [], [])
-# 0120 Projects
-# render("0120_Projects", 600, [], [])
+render("Check the dinner menu", "END_Check the dinner menu", [], [])
 
 
 print("######## END   ########")
