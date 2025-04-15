d18cc618eed0fe0fcc0a15bf19e7f9430dff0a2b
N(Natukikazemizo) <kmaeyama@ms4.megaegg.ne.jp>
2025-04-15 19:34:05 +0900

    UMLコネクタ学習用にコーディング

---
+++ src/java/study/class_diagram/connector/association/Arm.java
@@ -0,0 +1,12 @@
+package java.study.class_diagram.connector.simple_line;
+
+import java.study.class_diagram.connector.simple_line.Hand;
+
+/**
+ * 腕
+ * -------------------
+ * 
+ */
+public class Arm {
+    private Hand hand;
+}
