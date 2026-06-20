276d23bea38f3756ee3ed3b3c8d20c6dc0922671
N(Natukikazemizo) <kmaeyama@ms4.megaegg.ne.jp>
2026-06-20 09:30:16 +0900

    例外処理ソースに、パスとエンコード指定処理を追加

--- src/python/study/logger/output_exception.py
+++ src/python/study/logger/output_exception.py
@@ -1,14 +1,21 @@
 import logging
+import os
+
+# 保存したいログファイルの絶対パスまたは相対パスを作成
+log_path = os.path.join(os.path.dirname(__file__), 'app.log')
 
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s [%(levelname)s] %(name)s: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    datefmt='%Y/%m/%d %H:%M:%S',
+    filename=log_path,
+    encoding='utf-8'
 )
 
 logger = logging.getLogger(__name__)
 
 try:
+    logger.info(log_path)
     1 / 0
 except Exception:
     logger.exception("エラーが発生しました")   # これだけでOK！
